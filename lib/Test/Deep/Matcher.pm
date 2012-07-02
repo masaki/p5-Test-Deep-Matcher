afc750a3cb37ce1b78d6d999ca4f5bd4ea676597
ikasam_a <masaki.nakagawa@gmail.com>
2012-07-03 03:54:29 +0900

    initial commit

---
+++ lib/Test/Deep/Matcher.pm
@@ -0,0 +1,37 @@
+package Test::Deep::Matcher;
+
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+1;
+
+=head1 NAME
+
+Test::Deep::Matcher - A module that ...
+
+=head1 SYNOPSIS
+
+    use Test::Deep::Matcher;
+
+=head1 DESCRIPTION
+
+Test::Deep::Matcher is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+NAKAGAWA Masaki E<lt>masaki@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
